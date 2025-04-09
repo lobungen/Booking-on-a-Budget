4ac22c9e2945fe439bb63ef5bad644e1ed2ac22c
Lee Obungen <lobungen@github.com>
2025-04-08 20:11:18 -0500

    updated home-page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,21 @@
 import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>,
+);
+
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
 
 // Bringing in the pages the router will use to conditionally show the appropriate views
-import App from './App';
-import ErrorPage from './pages/ErrorPage';
-import LosAngelesMainPage from './pages/LosAngeles';
-// import HomePage from './pages/HomePage';
+import App from './app.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import LosAngelesMainPage from './pages/LosAngeles.jsx';
+import HomePage from './pages/HomePage';
 // import ProfilePage from './pages/ProfilePage';
 // import AboutPage from './pages/AboutPage';
 
