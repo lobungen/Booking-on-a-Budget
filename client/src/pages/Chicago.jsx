1f84c91be7ef292555b83a5b948763c71f25683d
AJ Kozlak II <ajkozlak2@AJs-MacBook-Pro-5.local>
2025-04-08 19:34:07 -0500

    individual cities jsx and updated html

---
+++ client/src/pages/Chicago.jsx
@@ -0,0 +1,16 @@
+function ChicagoMainPage() {
+    return (
+        <>
+        <h1>Chicago Main Page</h1>
+        <p>Welcome to the Chicago Main Page!</p>
+        <p>"The windy city is one of the most popular midwest tourist destinations. From the many sports teams to enjoy, to the original deep dish pizza and chicago dog restaraunts, and beautiful architecture and sights along the river, Chicago is a one-of-a-kind place that can be perfect for anyone."</p>
+        <div class="scrollable-list" id="destinationList">
+            <div class="destination" onclick="selectDestination('Chicago')">Chicago</div>
+            <img src="src/assets/chicago.jpg" alt="Chicago skyline." style="width:500px;height:300px;"/>
+            <img src="src/assets/deep-dish-pizza.jpg" alt="Deep dish pizza." style="width:500px;height:300px;"/>
+            <img src="src/assets/chicago-dog.jpg" alt="Chicago dog." style="width:500px;height:300px;"/>
+
+            </div>
+        </>
+    );
+}
---
+++ client/src/pages/Orlando.jsx
@@ -0,0 +1,15 @@
+function OrlandoMainPage() {
+    return (
+        <>
+        <h1>Orlando Main Page</h1>
+        <p>Welcome to the Orlando Main Page!</p>
+        <p>"Orlando, known for its theme parks and warm weather, offers a variety of attractions and activities for visitors of all ages."</p>
+        <div class="scrollable-list" id="destinationList">
+            <div class="destination" onclick="selectDestination('Orlando')">Orlando</div>
+            <img src="src/assets/orlando.jpg" alt="Orlando skyline." style="width:500px;height:300px;"/>
+            <img src="src/assets/universal-studios.jpg" alt="Universal Studios." style="width:500px;height:300px;"/>
+            <img src="src/assets/disney-world.jpg" alt="Disney World." style="width:500px;height:300px;"/>
+        </div>
+        </>
+    );
+}
